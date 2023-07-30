6bf2c8c64ba8ab20e8039a4d93106bb368ca891f
Alabere <111560684+Alabere-coder@users.noreply.github.com>
2023-07-30 18:45:40 +0100

    New branch (#1)
    * commit changes
    
    * updated
    
    * comment removed

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,15 +1,30 @@
-import { Input, Space } from "@arco-design/web-react";
+import { Link, Button, Input, Space } from "@arco-design/web-react";
 import Avatar, { genConfig } from "react-nice-avatar";
+import { IconDown } from '@arco-design/web-react/icon';
 
 const config = genConfig({ sex: "man", hairStyle: "mohawk" });
+const ButtonGroup = Button.Group;
 
 const Navbar = () => {
   return (
     <div className="border-b border-slate-800 p-3 ">
       <div className="container flex justify-between">
-        <h4 className="text-4xl text-white">Navbar</h4>
+        <Space>
+          <ButtonGroup>
+            <Button>Sax-Loader</Button>
+            <Button icon={<IconDown />} />
+          </ButtonGroup>
+  
+          <Link href="#">Overview</Link>
+          <Link href="#">Customers</Link>
+          <Link href="#">Products</Link>
+          <Link href="#">Settings</Link>
+        </Space>
+        
         <Space size={40}>
-          <Input className="w-72" />
+        
+           <Input className="w-72" placeholder="search..."/>
+          
           <Avatar className="w-8 h-8" {...config} />
         </Space>
       </div>
