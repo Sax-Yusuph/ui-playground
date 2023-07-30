2f8676e3897920e96c448bf99c2ec77284702865
sax-yusuph <yusuphshamsondeen@gmail.com>
2023-07-30 19:20:59 +0100

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Button, Card, Grid, Space } from "@arco-design/web-react";
+import { Button, Card, DatePicker, Grid, Space, Typography } from "@arco-design/web-react";
 import Navbar from "./components/ui/navbar";
 import { Overview } from "./components/ui/overview";
 import RecentSales from "./components/ui/recentSales";
-import { IconCalendar } from "@arco-design/web-react/icon";
 
 const Row = Grid.Row;
 const Col = Grid.Col;
+const { RangePicker } = DatePicker;
 
 interface StatsProps {
   title: string;
@@ -16,46 +16,41 @@ interface StatsProps {
 function App() {
   return (
     <div className="bg-slate-950">
+      <Navbar />
       <div className="flex flex-col h-[100vh] bg-slate-950 border-2 border-solid border-slate-950 m-4">
-        <Card bordered>
-          <Navbar />
+        <section className="container">
+          <Space direction="vertical" size={30} className="w-full mt-12">
+            <div className="flex justify-between">
+              <Typography.Title heading={1}>Dashboard</Typography.Title>
+              <Space>
+                <RangePicker />
+                <Button>Download</Button>
+              </Space>
+            </div>
 
-          <section className="container">
-            <Space direction="vertical" size={30} className="w-full mt-12">
-              <div className="flex justify-between">
-                <h1 className="text-white text-2xl ">Dashboard</h1>
-                <div className="flex"> 
-                  <div className="text-sm/[17px] mx-2 flex items-center justify-center border-2 border-solid border-gray-800 rounded-lg">
-                    <IconCalendar />
-                    <p className="text-sm/[14px]">Jan 20, 2023 - Feb 09, 2023</p>
-                  </div>
-                  <Button >Download</Button>
-                </div>
-              </div>
-              <Row gutter={20}>
-                <Stats title="Total Revenue" amount="$45,231.89" percentage="+20.1% from last month" />
-                <Stats title="Subscriptions" amount="+2350" percentage="+180.1% from last month" />
-                <Stats title="Sales" amount="+12,234" percentage="+19% from last month" />
-                <Stats title="Active Now" amount="+573" percentage="+201% since last hour" />
-              </Row>
+            <Row gutter={20}>
+              <Stats title="Total Revenue" amount="$45,231.89" percentage="+20.1% from last month" />
+              <Stats title="Subscriptions" amount="+2350" percentage="+180.1% from last month" />
+              <Stats title="Sales" amount="+12,234" percentage="+19% from last month" />
+              <Stats title="Active Now" amount="+573" percentage="+201% since last hour" />
+            </Row>
 
-              <Row gutter={20}>
-                <Col span={12}>
-                  <Card bordered>
-                    <h1 className="text-white py-4 pl-5">Overview</h1>
-                    <Overview />
-                  </Card>
-                </Col>
+            <Row gutter={20}>
+              <Col span={12}>
+                <Card bordered>
+                  <h1 className="text-white py-4 pl-5">Overview</h1>
+                  <Overview />
+                </Card>
+              </Col>
 
-                <Col span={12}>
-                  <Card bordered>
-                    <RecentSales />
-                  </Card>
-                </Col>
-              </Row>
-            </Space>
-          </section>
-        </Card>
+              <Col span={12}>
+                <Card bordered>
+                  <RecentSales />
+                </Card>
+              </Col>
+            </Row>
+          </Space>
+        </section>
       </div>
     </div>
   );
--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,11 +1,22 @@
-import { Link, Button, Input, Space } from "@arco-design/web-react";
+import { Button, Input, Link, Space } from "@arco-design/web-react";
+import { IconDown } from "@arco-design/web-react/icon";
 import Avatar, { genConfig } from "react-nice-avatar";
-import { IconDown } from '@arco-design/web-react/icon';
 
 const config = genConfig({ sex: "man", hairStyle: "mohawk" });
 const ButtonGroup = Button.Group;
 
 const Navbar = () => {
+  // return (
+  //   <div className="border-b border-slate-800 p-3 ">
+  //     <div className="container flex justify-between">
+  //       <h4 className="text-4xl text-white">Navbar</h4>
+  //       <Space size={40}>
+  //         <Input className="w-72" />
+  //         <Avatar className="w-8 h-8" {...config} />
+  //       </Space>
+  //     </div>
+  //   </div>
+  // );
   return (
     <div className="border-b border-slate-800 p-3 ">
       <div className="container flex justify-between">
@@ -14,17 +25,16 @@ const Navbar = () => {
             <Button>Sax-Loader</Button>
             <Button icon={<IconDown />} />
           </ButtonGroup>
-  
+
           <Link href="#">Overview</Link>
           <Link href="#">Customers</Link>
           <Link href="#">Products</Link>
           <Link href="#">Settings</Link>
         </Space>
-        
+
         <Space size={40}>
-        
-           <Input className="w-72" placeholder="search..."/>
-          
+          <Input className="w-72" placeholder="search..." />
+
           <Avatar className="w-8 h-8" {...config} />
         </Space>
       </div>
