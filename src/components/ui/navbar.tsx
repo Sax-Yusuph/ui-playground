b405f27789f38a3cbfe4ac1b839969f7d9b5659a
Alabere-coder <alabere4real247@gmail.com>
2023-07-31 08:36:50 +0100

    updated

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -4,7 +4,7 @@ import Avatar, { genConfig } from "react-nice-avatar";
 
 
 const config = genConfig({ sex: "man", hairStyle: "mohawk" });
-
+const TreeNode = TreeSelect.Node;
 const treeData = [
   {
     key: 'node1',
@@ -35,13 +35,13 @@ const treeData = [
 
 
 const Navbar = () => {
-  const [value, setValue] = useState({
-    value: 'node2',
-    label: <div className="flex items-center gap-x-2">
-      <Avatar className="w-4 h-4" {...config} />
-      <p>Sax-User1</p>
-    </div>,
-  });
+  // // const [value, setValue] = useState({
+  // //   value: 'node2',
+  // //   label: <div className="flex items-center gap-x-2">
+  // //     <Avatar className="w-4 h-4" {...config} />
+  // //     <p>Sax-User1</p>
+  // //   </div>,
+  // });
   return (
     <div className="border-b border-slate-800 p-3 ">
       <div className="container flex justify-between">
@@ -49,7 +49,7 @@ const Navbar = () => {
           
           <div className="border-b border-slate-800 p-3 ">
             <div className="flex flex-col">
-              <TreeSelect
+              {/* <TreeSelect
                 labelInValue={true}
                 treeData={treeData}
                 value={value}
@@ -64,7 +64,19 @@ const Navbar = () => {
                 }}
                 style={{ width: 300 }}
                 
-              />
+              /> */}
+              
+              <TreeSelect  defaultValue='node2' style={{ width: 300 }} allowClear onVisibleChange={() => {
+                console.log('a')
+              }}>
+                <TreeNode key='node1' title='Personal Account'>
+                  <TreeNode key='node2' title='Sax-User1' />
+                </TreeNode>
+                <TreeNode key='node3' title='Teams'>
+                  <TreeNode key='node4' title='Sax-User2' />
+                  <TreeNode key='node5' title='Sax-User3' />
+                </TreeNode>
+              </TreeSelect>
             
             </div>
           </div>
