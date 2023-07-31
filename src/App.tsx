f75db39ef55d1d35a70d412ff7121bd110849d7a
Alabere-coder <alabere4real247@gmail.com>
2023-07-31 12:39:39 +0100

    changes made

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,9 @@ import { Overview } from "./components/ui/overview";
 import RecentSales from "./components/ui/recentSales";
 import { IconCalendar } from "@arco-design/web-react/icon";
 
-import { Button, Card, DatePicker, Grid, Space, Typography } from "@arco-design/web-react";
-import Navbar from "./components/ui/navbar";
-import { Overview } from "./components/ui/overview";
-import RecentSales from "./components/ui/recentSales";
 
 const Row = Grid.Row;
 const Col = Grid.Col;
-const { RangePicker } = DatePicker;
-
-interface StatsProps {
-  title: string;
-  amount: string;
-  percentage: string;
-}
 
 interface StatsProps {
   title: string;
@@ -79,42 +68,6 @@ function App() {
             </Space>
           </section>
         </Card>
-    <div className="bg-slate-950">
-      <Navbar />
-      <div className="flex flex-col h-[100vh] bg-slate-950 border-2 border-solid border-slate-950 m-4">
-        <section className="container">
-          <Space direction="vertical" size={30} className="w-full mt-12">
-            <div className="flex justify-between">
-              <Typography.Title heading={1}>Dashboard</Typography.Title>
-              <Space>
-                <RangePicker />
-                <Button>Download</Button>
-              </Space>
-            </div>
-
-            <Row gutter={20}>
-              <Stats title="Total Revenue" amount="$45,231.89" percentage="+20.1% from last month" />
-              <Stats title="Subscriptions" amount="+2350" percentage="+180.1% from last month" />
-              <Stats title="Sales" amount="+12,234" percentage="+19% from last month" />
-              <Stats title="Active Now" amount="+573" percentage="+201% since last hour" />
-            </Row>
-
-            <Row gutter={20}>
-              <Col span={12}>
-                <Card bordered>
-                  <h1 className="text-white py-4 pl-5">Overview</h1>
-                  <Overview />
-                </Card>
-              </Col>
-
-              <Col span={12}>
-                <Card bordered>
-                  <RecentSales />
-                </Card>
-              </Col>
-            </Row>
-          </Space>
-        </section>
       </div>
     </div>
   );
