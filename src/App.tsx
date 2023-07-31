e1e4d3d85ed616af82423c2882757397d96ee92f
Alabere <111560684+Alabere-coder@users.noreply.github.com>
2023-07-31 12:46:46 +0100

    New pull (#2)
    * commit changes
    
    * updated
    
    * comment removed
    
    * tabs added
    
    * date picker added
    
    * updated
    
    * updated
    
    * updated
    
    * changes made

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { Button, Card, DatePicker, Grid, Space, Typography } from "@arco-design/web-react";
+import { Button, Card, Grid, Space, DatePicker } from "@arco-design/web-react";
 import Navbar from "./components/ui/navbar";
 import { Overview } from "./components/ui/overview";
 import RecentSales from "./components/ui/recentSales";
+import { IconCalendar } from "@arco-design/web-react/icon";
+
 
 const Row = Grid.Row;
 const Col = Grid.Col;
-const { RangePicker } = DatePicker;
 
 interface StatsProps {
   title: string;
@@ -13,44 +14,60 @@ interface StatsProps {
   percentage: string;
 }
 
+
 function App() {
   return (
-    <div className="bg-slate-950">
-      <Navbar />
+    <div className="bg-slate-950 ">
       <div className="flex flex-col h-[100vh] bg-slate-950 border-2 border-solid border-slate-950 m-4">
-        <section className="container">
-          <Space direction="vertical" size={30} className="w-full mt-12">
-            <div className="flex justify-between">
-              <Typography.Title heading={1}>Dashboard</Typography.Title>
-              <Space>
-                <RangePicker />
-                <Button>Download</Button>
-              </Space>
-            </div>
-
-            <Row gutter={20}>
-              <Stats title="Total Revenue" amount="$45,231.89" percentage="+20.1% from last month" />
-              <Stats title="Subscriptions" amount="+2350" percentage="+180.1% from last month" />
-              <Stats title="Sales" amount="+12,234" percentage="+19% from last month" />
-              <Stats title="Active Now" amount="+573" percentage="+201% since last hour" />
-            </Row>
-
-            <Row gutter={20}>
-              <Col span={12}>
-                <Card bordered>
-                  <h1 className="text-white py-4 pl-5">Overview</h1>
-                  <Overview />
+        <Card >
+          <Navbar />
+          
+          <section className="container">
+            <Space direction="vertical" size={30} className="w-full mt-8">
+              <div className="flex justify-between -mb-4">
+                <h1 className="text-white text-3xl ">Dashboard</h1>
+                <div className="flex"> 
+                  <div className="text-sm/[17px] mx-2 flex items-center justify-center border-2 border-solid border-gray-800 rounded-lg">
+                    <IconCalendar />
+                    <p className="text-sm/[14px]">
+                      <DatePicker.RangePicker style={{ width: 350 }} />
+                    </p>
+                  </div>
+                  <Button >Download</Button>
+                </div>
+              </div>
+              <div className="w-fit h-8 text-slate-50">
+                <Card bordered className='h-12 text-slate-50 flex justify-center items-center bg-stone-600'>
+                  <Button className=''>Overview</Button>
+                  <Button className='text-slate-50' >Analytics</Button>
+                  <Button  disabled>Reports</Button>
+                  <Button  disabled>Notifications</Button>
                 </Card>
-              </Col>
+              </div>
+              <Row gutter={20}>
+                <Stats title="Total Revenue" amount="$45,231.89" percentage="+20.1% from last month" />
+                <Stats title="Subscriptions" amount="+2350" percentage="+180.1% from last month" />
+                <Stats title="Sales" amount="+12,234" percentage="+19% from last month" />
+                <Stats title="Active Now" amount="+573" percentage="+201% since last hour" />
+              </Row>
 
-              <Col span={12}>
-                <Card bordered>
-                  <RecentSales />
-                </Card>
-              </Col>
-            </Row>
-          </Space>
-        </section>
+              <Row gutter={20}>
+                <Col span={12}>
+                  <Card bordered>
+                    <h1 className="text-white py-4 pl-5">Overview</h1>
+                    <Overview />
+                  </Card>
+                </Col>
+
+                <Col span={12}>
+                  <Card bordered>
+                    <RecentSales />
+                  </Card>
+                </Col>
+              </Row>
+            </Space>
+          </section>
+        </Card>
       </div>
     </div>
   );
--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,26 +1,39 @@
-import { Button, Input, Link, Space } from "@arco-design/web-react";
+import { Link, Button, Input, Space, TreeSelect } from "@arco-design/web-react";
 import { IconDown } from "@arco-design/web-react/icon";
 import Avatar, { genConfig } from "react-nice-avatar";
 
+
 const config = genConfig({ sex: "man", hairStyle: "mohawk" });
+const TreeNode = TreeSelect.Node;
+
 const ButtonGroup = Button.Group;
 
 const Navbar = () => {
-  // return (
-  //   <div className="border-b border-slate-800 p-3 ">
-  //     <div className="container flex justify-between">
-  //       <h4 className="text-4xl text-white">Navbar</h4>
-  //       <Space size={40}>
-  //         <Input className="w-72" />
-  //         <Avatar className="w-8 h-8" {...config} />
-  //       </Space>
-  //     </div>
-  //   </div>
-  // );
+
   return (
     <div className="border-b border-slate-800 p-3 ">
       <div className="container flex justify-between">
         <Space>
+          
+          <div className="border-b border-slate-800 p-3 ">
+            <div className="flex flex-col">
+            
+              
+              <TreeSelect  defaultValue='node2' style={{ width: 300 }} allowClear onVisibleChange={() => {
+                console.log('a')
+              }}>
+                <TreeNode key='node1' title='Personal Account'>
+                  <TreeNode key='node2' title='Sax-User1' />
+                </TreeNode>
+                <TreeNode key='node3' title='Teams'>
+                  <TreeNode key='node4' title='Sax-User2' />
+                  <TreeNode key='node5' title='Sax-User3' />
+                </TreeNode>
+              </TreeSelect>
+            
+            </div>
+          </div>
+  
           <ButtonGroup>
             <Button>Sax-Loader</Button>
             <Button icon={<IconDown />} />
