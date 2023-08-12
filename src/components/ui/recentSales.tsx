a24ab26df0af4dd4b57898fa7a6dc16c209bc24f
Alabere <111560684+Alabere-coder@users.noreply.github.com>
2023-08-12 01:18:06 +0100

    new files from branch-b (#3)
    * commit changes
    
    * updated
    
    * comment removed
    
    * tabs added
    
    * date picker added
    
    * updated
    
    * updated
    
    * updated
    
    * changes made
    
    * team-switch and userNav fixed
    
    * team-switch and userNav fixed
    
    * comment removed
    
    * comment removed

--- src/components/ui/recentSales.tsx
+++ src/components/ui/recentSales.tsx
@@ -1,4 +1,3 @@
-import { Space } from "@arco-design/web-react";
 import NiceAvatar, { genConfig } from "react-nice-avatar";
 
 const myConfig = genConfig({ "sex": "man",
@@ -21,51 +20,64 @@ const myConfig = genConfig({ "sex": "man",
 const RecentSales = () => {
   return (
     <div>
-      <h1 className="text-white">Recent Sales</h1>
-      <p>You made 265 sales this month.</p>
-      
-      <Space direction="vertical" >
-        <div className="mt-5 flex space-x-6">
+      <h1 className="text-xl mb-1 text-white">Recent Sales</h1>
+      <p className="text-sm mb-8">You made 265 sales this month.</p>
+      <div className="space-y-8">
+        <div className="flex items-center">
+          <div className="h-9 w-9">
           <NiceAvatar style={{ width: '2rem', height: '2rem' }} {...myConfig} />
-          <div className="text-sm">
-            <h3 className="text-white">Olivia Martin</h3>
-            <p className="text-sm/[14px]">olivia.martin@email.com</p>
           </div>
-          <p className="pt-3 pl-64">+1,999.00</p>
+          <div className="ml-4 space-y-1">
+            <p className="text-sm font-medium leading-none">Olivia Martin</p>
+            <p className="text-sm text-muted-foreground">
+              alabere.coder@email.com
+            </p>
+          </div>
+          <div className="ml-auto font-medium">+$1,999.00</div>
         </div>
-        <div className="mt-4 flex space-x-6">
+        <div className="flex items-center">
+          <div className="flex h-9 w-9 items-center justify-center space-y-0">
           <NiceAvatar style={{ width: '2rem', height: '2rem' }} {...myConfig} />
-          <div>
-            <h3 className="text-white">Olivia Martin</h3>
-            <p className="text-sm/[14px]">olivia.martin@email.com</p>
           </div>
-          <p className="pt-3 pl-64">+1,999.00</p>
+          <div className="ml-4 space-y-1">
+            <p className="text-sm font-medium leading-none">Olivia Martin</p>
+            <p className="text-sm text-muted-foreground">alabere.coder@email.com</p>
+          </div>
+          <div className="ml-auto font-medium">+$39.00</div>
         </div>
-        <div className="mt-4 flex space-x-6">
+        <div className="flex items-center">
+          <div className="h-9 w-9">
           <NiceAvatar style={{ width: '2rem', height: '2rem' }} {...myConfig} />
-          <div>
-            <h3 className="text-white">Olivia Martin</h3>
-            <p className="text-sm/[14px]">olivia.martin@email.com</p>
           </div>
-          <p className="pt-3 pl-64">+1,999.00</p>
+          <div className="ml-4 space-y-1">
+            <p className="text-sm font-medium leading-none">Olivia Martin</p>
+            <p className="text-sm text-muted-foreground">
+              alabere.coder@email.com
+            </p>
+          </div>
+          <div className="ml-auto font-medium">+$299.00</div>
         </div>
-        <div className="mt-4 flex space-x-6">
+        <div className="flex items-center">
+          <div className="h-9 w-9">
           <NiceAvatar style={{ width: '2rem', height: '2rem' }} {...myConfig} />
-          <div>
-            <h3 className="text-white">Olivia Martin</h3>
-            <p className="text-sm/[14px]">olivia.martin@email.com</p>
           </div>
-          <p className="pt-3 pl-64">+1,999.00</p>
+          <div className="ml-4 space-y-1">
+            <p className="text-sm font-medium leading-none">Olivia Martin</p>
+            <p className="text-sm text-muted-foreground">alabere.coder@email.com</p>
+          </div>
+          <div className="ml-auto font-medium">+$99.00</div>
         </div>
-        <div className="mt-4 flex space-x-6">
+        <div className="flex items-center">
+          <div className="h-9 w-9">
           <NiceAvatar style={{ width: '2rem', height: '2rem' }} {...myConfig} />
-          <div>
-            <h3 className="text-white">Olivia Martin</h3>
-            <p className="text-sm/[14px]">olivia.martin@email.com</p>
           </div>
-          <p className="pt-3 pl-64">+1,999.00</p>
+          <div className="ml-4 space-y-1">
+            <p className="text-sm font-medium leading-none">Olivia Martin</p>
+            <p className="text-sm text-muted-foreground">alabere.coder@email.com</p>
+          </div>
+          <div className="ml-auto font-medium">+$39.00</div>
         </div>
-      </Space>
+      </div>
     </div>
   )
 }
